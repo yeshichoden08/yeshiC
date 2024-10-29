911f9bdb1fa831560556f7e5f4b8f91307c34af4
yeshichoden08 <yesheyychoden909@gmail.com>
2024-10-29 16:28:21 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scrolling
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      document.getElementById(targetId).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
